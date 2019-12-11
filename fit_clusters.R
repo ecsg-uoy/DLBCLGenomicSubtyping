71afd0922f26ba6b7207e1f6705ef5475544992b
Stuart Lacy <stuart.lacy@gmail.com>
2019-12-11 16:52:11 +0000

    Added ability to assign clusters to new data

--- fit_clusters.R
+++ fit_clusters.R
@@ -15,13 +15,18 @@ library(flexmix)
 library(tidyverse)
 library(gridExtra)
 
+######################################
+#   Fit original clusters
+######################################
+# This first step fits the published clusters using the supplied processed genomic data
+
 # Load library functions used for identifying enriched mutations and plotting heatmaps
 source("plotting_functions.R")
 
-# Load data
+# Load data (NB: this is also available in the supplementary material provided with this publication)
 muts_all <- read.csv("data/genomic_data.csv")
 
-# Have 928 patients with 118 columns. First column is identifier, remaining 117 are genetic features
+# There are 928 patients with 118 columns. First column is identifier, remaining 117 are genetic features
 dim(muts_all)
 
 # Remove patient id, restricting columns to 117 mutations
@@ -32,12 +37,16 @@ dim(muts_df)
 # They set the seed to the same used in the paper
 RNGversion("3.5.3")
 set.seed(17)
-# This goes through all the possible cluster numbers from 1 to 10 (k = 1:10).
-# and runs a thousand reps of each and selects the best
+
+# Run the mixture model fitting algorithm through all the possible cluster numbers from 1 to 10,
+# running a thousand repetitions of each and selecting the best
 ex <- initFlexmix(as.matrix(muts_df) ~ 1,
                   k = 1:10, model = FLXMCmvbinary(),
                   control = list(minprior = 0.10), nrep = 1000)
 
+######################################
+#   Visualise clusters
+######################################
 # Visualise number of clusters suggested by AIC and BIC/ICL (6 and 4 respectively)
 plot(ex)
 
@@ -57,4 +66,51 @@ grid.arrange(plt_aic)
 plt_icl <- heatmap_mutation_extended(muts_all, genes, 'ClusterICL', y_order = 'group', idcol = 'PID')
 grid.arrange(plt_icl)
 
-# TODO Produce function to predict membership for new patients
+######################################
+#   Assign new samples to clusters
+######################################
+# This section demonstrates how to input new data into the mixture model to view
+# where they would be assigned.
+
+# Function to predict membership for new patients
+# Args:
+#    - model: A flexmix object.
+#    - newdata: Data frame with the same number of columns (117) as muts_df and in the same order.
+#
+# Returns:
+#   - A matrix where each row corresponds to a row of newdata and each column provides the probability
+#     of belonging to that cluster. Dimensions NxC where N = nrow(newdata) and C = number of clusters
+#     that the model identified
+# newdata:
+predict_clusters <- function(model, newdata) {
+    # Flexmix looks for a data frame with the same name as that used to
+    # fit the model in the first place, so we need to overwrite this
+    # variable name at the higher scope
+    muts_df <<- newdata
+    probs <- flexmix::posterior(model, newdata=newdata)
+    probs
+}
+
+# Predicting cluster for just first 10 samples in dataset
+# Having to copy original data frame into new variable as the
+# prediction process will overwrite this variable
+orig_df <- muts_df
+test_data <- orig_df[1:10, ]
+predict_clusters(aic, test_data)
+
+# BIC model returns 4 columns
+predict_clusters(bic, test_data)
+
+# View predicted cluster membership for an imaginary sample.
+# This will use as an example a sample with a number of mutations associated with MYD88
+
+# This line creates a 1 row data frame with the same columns as muts_df, all set to 0 (no mutation)
+new_df <- as.data.frame(setNames(lapply(genes, function(x) 0), genes))
+# Individual mutations can be set
+new_df$MYD88_265 <- 1
+new_df$PIM1_S <- 1
+new_df$CD79B <- 1
+
+# From the heatmap it can be observed that MYD88 is the 5th cluster, which is consistent
+# with the predicted 93% assignment for this imaginary sample.
+predict_clusters(aic, new_df)
